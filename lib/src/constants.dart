688b354440b6e8bdbfcb8235aaec5c75821231ad
Márcio Rossato <marcioarp@gmail.com>
2020-11-17 00:57:09 -0300

    Windws Init

---
+++ lib/src/constants.dart
@@ -0,0 +1,13 @@
+class FRBorderStyle {
+  FRBorderStyle._();
+  static const bsNone = 'none';
+  static const bsSolid = 'solid';
+  static const bsDashed = 'dashed';
+}
+
+class FRGenerateOn {
+  FRGenerateOn._();
+  static const goAllData = 'allData';
+  static const goFirstData = 'firstData';
+  static const goLastData = 'lastData';
+}
